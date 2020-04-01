415476730bdfec3262ea651549495a3723bbddbc
Luis <corguerraluis@gmail.com>
2020-04-01 20:47:59 +0200

    Ejercicios de repaso 2ª evaluación - Matrices Revision 1

--- src/repaso2ev/Matrices.java
+++ src/repaso2ev/Matrices.java
@@ -4,155 +4,196 @@ public class Matrices {
 
 	// Ejercicio de Repaso 2ª Evaluación - Matrices CIFP Avilés 1º DAW
 	// Programación Clase Matrices
-	//  
+	//
 	// Created by Luis Cortes. Date 26-03-2020
-	
-	
-	
-	//Metodo de clase cuadrada1
-	//Como entrada es un número entero y salida va a ser una matriz de dos dimensiones, con valores enteros.
+
+	// Modified 1 by Luis Cortes. Date 26-03-2020
+	// Se modifica en Metodo palindromo, en relación al abecedario
+	// Se elimina el vector y se cogeran las letras por su codigo ACCII,
+	// Para el caracter cetral tendremos en cuenta minusculas y mayusculas para que
+	// la suma pueda ser de 26+25 = 51 elementos
+
+	// Metodo de clase cuadrada1
+	// Como entrada es un número entero y salida va a ser una matriz de dos
+	// dimensiones, con valores enteros.
 	static int[][] cuadrada1(int dim) {
 
 		int matriz[][] = new int[dim][dim]; // Se crea el objeto Matriz de tipo entera la dimensión dim dim
-		int valor = 1; // Declaramos una variable de tipo entera y le damos el valor 0, que sea el valor que va tomando cada elemento de la matriz  
-		
-		// Hacemos un bucle recorriendo la matriz he introducciendo los valores en cada elemento
-		
+		int valor = 1; // Declaramos una variable de tipo entera y le damos el valor 0, que sea el
+						// valor que va tomando cada elemento de la matriz
+
+		// Hacemos un bucle recorriendo la matriz he introducciendo los valores en cada
+		// elemento
+
 		for (int j = 0; j < dim; j++) {
 			for (int i = 0; i < dim; i++) {
-		//Recorremos la matriz empezando por el el elemento F 0 C 0, nos vamos moviendo dentro de cada columna por las filas que tiene y luego cambiamos de columna y recorremos otra vez sus filas.... 
+				// Recorremos la matriz empezando por el el elemento F 0 C 0, nos vamos moviendo
+				// dentro de cada columna por las filas que tiene y luego cambiamos de columna y
+				// recorremos otra vez sus filas....
 				matriz[i][j] = valor;
-				
-				//El valor va incrementandose en 1 cada vez que cambiamos de elemento 
+
+				// El valor va incrementandose en 1 cada vez que cambiamos de elemento
 				valor = valor + 1;
 			}
 		}
 
 		// retornamos la matriz con todos los valores de sus elementos
-		return matriz; 
-
-	}
-	
-	
-	
-	//Metodo de clase cuadrada1
-	//Como entrada es un número entero y salida va a ser una matriz de dos dimensiones, con valores enteros.
-	static int[][] cuadrada2(int dim) {
-
-		int matriz[][] = new int[dim][dim];// Se crea el objeto Matriz de tipo entera la dimensión dim dim
-		int valor = 1;// Declaramos una variable de tipo entera y le damos el valor 0, que sea el valor que va tomando cada elemento de la matriz  
-		
-		
-		//En este caso dependiendo si la columna es par o impar, recorremos al matriz de abajo a arriba o de arriba a abajo
-		
-		//Empezamos en la columna 0 hasta su longitud-1, en este caso es el parametro introducido-1
-		for (int j = 0; j < dim; j++) {
-			
-			// valoramos si esa columna es par o impar,
-			
-			//Si es par
-			if (j % 2 == 0) {
-
-				//recorremos la columna, de la fila 0 hasta su longitud
-				for (int i = 0; i < dim; i++) {
-
-					// y metemos el valor de la variable que tiene en ese momento 
-					matriz[i][j] = valor;
-					
-					//incrementamos en 1 el valor de la variable en cada iteración
-					valor = valor + 1;
-				}
-			//Sino es par la columna	
-			} else {
-				
-				//recorremos dentro de cada columna, desde la ultima fila (dim-1) hasta 0 en el ejemplo de abajo a arriba 
-				for (int i = dim - 1; i >= 0; i--) {
-					
-					// y metemos el valor de la variable que tiene en ese momento
-					matriz[i][j] = valor;
-					//incrementamos en 1 el valor de la variable en cada iteración
-					valor = valor + 1;
-				}
-			}
-		}
-		//retornamos la matriz con todos los valores de sus elementos
 		return matriz;
 
 	}
 
-	
-	
-	
-	//Metodo de clase palindromo
-	//Como entrada son dos números enteros ques serán las dimensiones de una matriz y salida
-	//Retornara o bien una matriz de tipo String con los palindromos o bien retronará null
-	
+	// Metodo de clase cuadrada1 VERSION 1 01-04-2020
+	// Como entrada es un número entero y salida va a ser una matriz de dos
+	// dimensiones, con valores enteros.
+	static int[][] cuadrada2(int dim) {
+
+		int matriz[][] = new int[dim][dim];// Se crea el objeto Matriz de tipo entera la dimensión dim dim
+		int valor = 1;// Declaramos una variable de tipo entera y le damos el valor 0, que sea el
+						// valor que va tomando cada elemento de la matriz
+
+		// En este caso dependiendo si la columna es par o impar, recorremos al matriz
+		// de abajo a arriba o de arriba a abajo
+
+		// Empezamos en la columna 0 hasta su longitud-1, en este caso es el parametro
+		// introducido-1
+		for (int j = 0; j < dim; j++) {
+
+			// valoramos si esa columna es par o impar,
+
+			// Si es par
+			if (j % 2 == 0) {
+
+				// recorremos la columna, de la fila 0 hasta su longitud
+				for (int i = 0; i < dim; i++) {
+
+					// y metemos el valor de la variable que tiene en ese momento
+					matriz[i][j] = valor;
+
+					// incrementamos en 1 el valor de la variable en cada iteración
+					valor = valor + 1;
+				}
+				// Sino es par la columna
+			} else {
+
+				// recorremos dentro de cada columna, desde la ultima fila (dim-1) hasta 0 en el
+				// ejemplo de abajo a arriba
+				for (int i = dim - 1; i >= 0; i--) {
+
+					// y metemos el valor de la variable que tiene en ese momento
+					matriz[i][j] = valor;
+					// incrementamos en 1 el valor de la variable en cada iteración
+					valor = valor + 1;
+				}
+			}
+		}
+		// retornamos la matriz con todos los valores de sus elementos
+		return matriz;
+
+	}
+
+	// Metodo de clase palindromo
+	// Como entrada son dos números enteros ques serán las dimensiones de una matriz
+	// y salida
+	// Retornara o bien una matriz de tipo String con los palindromos o bien
+	// retronará null
+
 	static String[][] palindromo(int d, int f) {
 
-		
-		// Como vamos a trabajar con una vector que va a tener 26 elementos solo podemos hacer operaciones
-		//Con filas y columnas de dimension maxima 26 y como maximo 27 la suma de las dos, por ejemplo 1 fila con 26 columnas
-		
-		// Entonces si se cumplen estos requisitos
-		if ((d >= 1 && d <= 26) && (f >= 1 && f <= 26) && (d + f <= 27)) {
-			
+		/**
+		 * La dimensión maxima de filas y columnas tiene que estar comprnedido entre el
+		 * 1 y el 26 Entonces si se cumplen estos requisitos,
+		 */
+
+		if ((d >= 1 && d <= 26) && (f >= 1 && f <= 26)) {
+
 			// Se crea un objeto matriz de tipo cadena de texto
+
 			String matriz[][] = new String[d][f];
-			
-			//creamos un vector con las letras del abecedario, excluimos la ñ, son 26 
-			char[] abecedario = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q',
-					'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
-			
-			//Recorremos la matriz fila --> columna
+
+			/**
+			 * VERSION 1 El valor del primera y tercera letra, dentro de cada referencia de
+			 * la matriz es el valor del codigo ASCII de la referencia de fila en la que
+			 * estemos sumandole 97 (que es el codigo ASCII de la a misnuscula) y el valor
+			 * de cada uno de sus sucesivos numeros hasta el codigo 122 es el valor de cada
+			 * una de las letras del abecedario ordenadas alfabeticamente.
+			 * 
+			 * Para saber el valor de un codigo ASCII utilizaremos "(Char) codigoASCII"
+			 * 
+			 * 
+			 * Para el caso de la segunda letra, como está determinado por la suma de la
+			 * fila y la columna Necesitaremos 26 + 25 elementos por lo tanto meteremos en
+			 * juego al abecederario en letras minusculas (26) y en letras MAYUSCULAS (25),
+			 * de tal forma que si la suma de si la suma es entre 0 a 25 (26 elementos)
+			 * cogeremos el valor de esta suma + 97, si la suma de la referencia de la fila
+			 * y columna es etre 26 y 51 incluidos cogeremos el valor de la suma de la fila
+			 * y columna sumandole 39 ( en realidad usaremos if como condicion la primera y
+			 * un else si no cuemple la primera como el primer codigo AXCII devido a que las
+			 * letras mayusculas empiezan en el codigo ASCII 65. Si la suma es 26 entonces
+			 * para llegar a 65 deberemos sumarle 39.
+			 * 
+			 */
+
+			// Recorremos la matriz fila --> columna
 			for (int i = 0; i < d; i++) {
 				for (int j = 0; j < f; j++) {
 
-					//cada valor de cada referencia de la matriz es igual a
-					//Primer y tercer caracter de esa referencia, es el valor de esa misma referencia en vector abecedario
-					//para el segundo caracter de esa referencia, es el valor que tiene en vector la referencia igual a la suma de la referencias de fila y columna de la matriz
-					matriz[i][j] = (String.valueOf(abecedario[i]) + String.valueOf(abecedario[i + j])
-							+ String.valueOf(abecedario[i]));
+					// Calculamos el codigo ASCII para el segundo valor
+
+					int codigoASCII; // variable que sera el codigo ASCII de la segunda letra
+
+					if (i + j <= 25) {
+						codigoASCII = i + j + 97;
+					} else {
+						codigoASCII = i + j + 39;
+					}
+
+					// imprimimos la Matriz
+					matriz[i][j] = (String.valueOf((char) (i + 97)) + String.valueOf((char) (codigoASCII))
+							+ String.valueOf((char) (i + 97)));
 
 				}
 			}
 
-			//Retornamos la matriz con palindormos creada
+			// Retornamos la matriz con palindormos creada
 			return matriz;
 		}
-		
+
 		// Sino cumplio los requisitos rertamos valor null
 		return null;
 	}
 
-	
-
-	//Metodo de clase max3x3sum
-	//Como entrada tenemos una matriz de dimension 2 de numeros entreros y salida retornamos un numero entero
+	// Metodo de clase max3x3sum
+	// Como entrada tenemos una matriz de dimension 2 de numeros entreros y salida
+	// retornamos un numero entero
 
 	static int max3x3sum(int[][] matriz) {
 
-        //declarmaos un parametro que sera la suma maxima que retornara el metodo en principo vale 0, 
+		// declarmaos un parametro que sera la suma maxima que retornara el metodo en
+		// principo vale 0,
 		int sumaMax = 0;
-		//Hacemos cuatro bucles
-		
-		//empezamos por la fila 0 hasta la fila de referencia (la longitud - 3 ) 
-		//las referencias van llegan hasta longitud-1 y la referencia inicial maxima es longitud-1-2 para luego porder coger 3 digitos 
-		
+		// Hacemos cuatro bucles
+
+		// empezamos por la fila 0 hasta la fila de referencia (la longitud - 3 )
+		// las referencias van llegan hasta longitud-1 y la referencia inicial maxima es
+		// longitud-1-2 para luego porder coger 3 digitos
+
 		for (int i = 0; i <= (matriz.length - 3); i++) {
-			
-			//dentro de cada fila recorreremos cada columna desde la columna 0 hasta (la longitud -3 ) 
+
+			// dentro de cada fila recorreremos cada columna desde la columna 0 hasta (la
+			// longitud -3 )
 			for (int j = 0; j <= (matriz[i].length - 3); j++) {
 				// cuando estamos en la primera referencia tendremos que empezar a sumar
-				//declaramos una variable entera que será la suma parcial que vamos obteniendo 
+				// declaramos una variable entera que será la suma parcial que vamos obteniendo
 				int suma = 0;
-				
+
 				// recorremos desde fila de la referencia anterior por fila (3 filas)
-				for (int f = i; f<= (i + 2); f++) {
-					
-					//en cada fila recorremos cada columna, empezando por la primera (3 columnas)
+				for (int f = i; f <= (i + 2); f++) {
+
+					// en cada fila recorremos cada columna, empezando por la primera (3 columnas)
 					for (int c = j; c <= (j + 2); c++) {
 
-						//a la variable suma le vamos incrementando el valor que tiene cada referencia de el recorrido de la matriz de 3x3
+						// a la variable suma le vamos incrementando el valor que tiene cada referencia
+						// de el recorrido de la matriz de 3x3
 						suma = suma + matriz[f][c];
 					}
 				}
@@ -161,12 +202,13 @@ public class Matrices {
 				// A la suma Maxima le damos el valor de la suma.
 				if (suma > sumaMax) {
 					sumaMax = suma;
-				
+
 				}
 			}
 		}
-		
-		//Despues de hacer los calculos recorriendo toda la matriz en matrices de 3x3 retornamos la Suma Maxima
+
+		// Despues de hacer los calculos recorriendo toda la matriz en matrices de 3x3
+		// retornamos la Suma Maxima
 		return sumaMax;
 	}
 }
--- src/repaso2ev/PruebaMatrices.java
+++ src/repaso2ev/PruebaMatrices.java
@@ -4,11 +4,14 @@ import java.util.Scanner;
 import java.util.Random;
 
 //Ejercicio de Repaso 2ª Evaluación - Matrices CIFP Avilés 1º DAW
-	// Programación PruevaMatrices
-	//  
-	// Created by Luis Cortes. Date 26-03-2020
-
-
+// Programación PruevaMatrices
+//  
+// Created by Luis Cortes. Date 26-03-2020
+//Modified 1 by Luis Cortes. Date 26-03-2020
+// Se modifica en Metodo palindromo, en relación al abecedario
+// Se elimina el vector y se cogeran las letras por su codigo ACCII,
+// Para el caracter cetral tendremos en cuenta minusculas y mayusculas para que
+// la suma pueda ser de 26+25 = 51 elementos
 
 public class PruebaMatrices {
 
@@ -17,13 +20,13 @@ public class PruebaMatrices {
 
 	public static void main(String[] args) {
 		int dimension;
-		
-		//Prueba "Metodo Cuadrada1
+
+		// Prueba "Metodo Cuadrada1
 		System.out.print("Metodo Cuadrada1, introduzca numero como dimensión de la matriz: ");
 		dimension = Integer.valueOf(input.nextLine());
 
 		int[][] mimatrizcuadrada1 = Matrices.cuadrada1(dimension); // llamaamos al metodo que nos devuelve la matriz
-		
+
 		// recorremos la matriz devuelta y la imprimimos por consola
 		for (int i = 0; i < mimatrizcuadrada1.length; i++) {
 			for (int j = 0; j < mimatrizcuadrada1[i].length; j++) {
@@ -31,14 +34,14 @@ public class PruebaMatrices {
 			}
 			System.out.println(); // Imprime salto de línea
 		}
-        
-		//Prueba "Metodo Cuadrada2
+
+		// Prueba "Metodo Cuadrada2
 		System.out.println(); // Imprime salto de línea
 		System.out.print("Metodo Cuadrada2, introduzca numero como dimensión de la matriz: ");
 		dimension = Integer.valueOf(input.nextLine());
 
-		int[][] mimatrizcuadrada2 = Matrices.cuadrada2(dimension); //llamamos al metodo y nos retorna la matriz
-		
+		int[][] mimatrizcuadrada2 = Matrices.cuadrada2(dimension); // llamamos al metodo y nos retorna la matriz
+
 		// recorremos la matriz devuelta y la imprimimos por consola
 		for (int i = 0; i < mimatrizcuadrada2.length; i++) {
 			for (int j = 0; j < mimatrizcuadrada2[i].length; j++) {
@@ -47,30 +50,31 @@ public class PruebaMatrices {
 			System.out.println(); // Imprime salto de línea
 		}
 
-		
-		//Prueba "Metodo Palindromo
+		// Prueba "Metodo Palindromo
 		System.out.println(); // Imprime salto de línea
 		System.out.println("Metodo palindromo:");
-		
-		//Pedimos el numero de filas y columnas advertimos de que no se puede exceder deciertos valores
-		//Pero no hacemos nada sino se cumplen para que el metodo nos devuelva el valor null si procede
-		System.out.println("El numero de filas y columnas debe ir entre 1 y 26, y su suma no puede ser superior a 27");
+
+		// Pedimos el numero de filas y columnas advertimos de que no se puede exceder
+		// de ciertos valores
+		// Pero no hacemos nada sino se cumplen para que el metodo nos devuelva el valor
+		// null si procede
+		System.out.println("El numero de filas y columnas debe estar entre 1 y 26");
 		System.out.print("Introduzca el número de filas de la Matriz: ");
 		int nfilas = Integer.valueOf(input.nextLine());
 
 		System.out.print("Introduzca el número de columnas de la Matriz: ");
 		int ncolumnas = Integer.valueOf(input.nextLine());
 
-		String[][] mimatrizpalindromo = Matrices.palindromo(nfilas, ncolumnas);// Llamamos al metodo 
+		String[][] mimatrizpalindromo = Matrices.palindromo(nfilas, ncolumnas);// Llamamos al metodo
 
-		//Si nos retorno null
+		// Si nos retorno null
 		if (mimatrizpalindromo == null) {
 
 			System.out.println("La matriz con palindromos no se puede realizar "); // Imprime salto de línea
 
 		} else {
-        //Sino retorn0 null, nos devolvio una matriz con palindronos.
-			
+			// Sino retorn0 null, nos devolvio una matriz con palindronos.
+
 			// recorremos la matriz devuelta y la imprimimos por consola
 			for (int i = 0; i < mimatrizpalindromo.length; i++) {
 				for (int j = 0; j < mimatrizpalindromo[i].length; j++) {
@@ -79,59 +83,61 @@ public class PruebaMatrices {
 				System.out.println(); // Imprime salto de línea
 			}
 		}
-				
-		//Prueba Metodo max3x3sum
+
+		// Prueba Metodo max3x3sum
 		System.out.println(); // Imprime salto de línea
 		System.out.println("Metodo max3x3sum: ");
 		int Nfilas;
 		int Mcolumnas;
-		
-		
-		//Vamos a solitar el numero de filas hasta que sea un número mayor o igual a 3
+
+		// Vamos a solitar el numero de filas hasta que sea un número mayor o igual a 3
 		do {
-		System.out.print("Introduzca número mayor o igual a 3 como filas de la matriz: ");
-		Nfilas = Integer.valueOf(input.nextLine());
-		} while(!(Nfilas>=3));
-		
-		
-		//Vamos a solitar el numero de columnas hasta que sea un número mayor o igual a 3
+			System.out.print("Introduzca número mayor o igual a 3 como filas de la matriz: ");
+			Nfilas = Integer.valueOf(input.nextLine());
+		} while (!(Nfilas >= 3));
+
+		// Vamos a solitar el numero de columnas hasta que sea un número mayor o igual a
+		// 3
 		do {
-		System.out.print("Introduzca número mayor o igual a 3 como columnas de la matriz: ");
-		Mcolumnas = Integer.valueOf(input.nextLine());
-		} while(!(Mcolumnas>=3));
-		
-		//Cremaos una matriz con la filas y columnas introduccidas anteriromente
-		int[][] matrizNxM = new int [Nfilas][Mcolumnas];
-		
-		// Para este ejemplo la matriz se llena mediante un metodo donde rellena la matriz
-		//Con numeros aleatorios desde el 0 hasta el 99
-		
-		
-		llenarMatriz (matrizNxM);
-		
+			System.out.print("Introduzca número mayor o igual a 3 como columnas de la matriz: ");
+			Mcolumnas = Integer.valueOf(input.nextLine());
+		} while (!(Mcolumnas >= 3));
+
+		// Cremaos una matriz con la filas y columnas introduccidas anteriromente
+		int[][] matrizNxM = new int[Nfilas][Mcolumnas];
+
+		// Para este ejemplo la matriz se llena mediante un metodo donde rellena la
+		// matriz
+		// Con numeros aleatorios desde el 0 hasta el 99
+
+		llenarMatriz(matrizNxM);
+
 		// recorremos la matriz original creada y la imprimimos por consola
 		for (int i = 0; i < matrizNxM.length; i++) {
 			for (int j = 0; j < matrizNxM[i].length; j++) {
 				System.out.print(matrizNxM[i][j] + " "); // Imprime Matriz
-					
+
 			}
 			System.out.println(); // Imprime salto de línea
 		}
-		
-		//llamamos al metodo y nos devuelve la mayor suma que se obtiene de todos los resultados, sumando los elementos de cada matriz de 3 x 3 que contiene la matriz original
-		int SumMax =  Matrices.max3x3sum(matrizNxM);
-		
-		// Imprimimimos el valor maximo 
-		System.out.println("La mayor suma de todos los elementos de cada una de las matrices de 3x3 contenidas en esta matriz es = "+ SumMax); 
-		
-	    
+
+		// llamamos al metodo y nos devuelve la mayor suma que se obtiene de todos los
+		// resultados, sumando los elementos de cada matriz de 3 x 3 que contiene la
+		// matriz original
+		int SumMax = Matrices.max3x3sum(matrizNxM);
+
+		// Imprimimimos el valor maximo
+		System.out.println(
+				"La mayor suma de todos los elementos de cada una de las matrices de 3x3 contenidas en esta matriz es = "
+						+ SumMax);
+
 	}
-	
+
 // Metodo que rellena una matriz de cualquier dimension con valores de numeros aleatorios entre el 0 y 99	
-  static void llenarMatriz (int [][] m) {
-	for (int i=0; i<m.length; i++)
-		for(int j=0; j<m[i].length; j++)
-		m[i][j] = r.nextInt(100);
-	
- }
+	static void llenarMatriz(int[][] m) {
+		for (int i = 0; i < m.length; i++)
+			for (int j = 0; j < m[i].length; j++)
+				m[i][j] = r.nextInt(100);
+
+	}
 }
